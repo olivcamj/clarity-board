dcc8408b2f56aaeb63c8dcdbe732f598981373c6
Olivia Cameron <olivia.cameronj15@gmail.com>
2025-09-22 18:54:07 -0400

    fix: wrap entry fn in try/catch

--- src/api/src/main.ts
+++ src/api/src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  try {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(process.env.PORT ?? 3000);
+  } catch (e) {
+    console.error(e);
+  }
 }
 bootstrap();
