3e6dbd82ef35cc6451b85140326a8fc8eeb4a148
Olivia Cameron <olivia.cameronj15@gmail.com>
2025-10-22 12:19:05 -0400

    feat (api): prisma setup

--- src/api/src/prisma/prisma.service.ts
+++ src/api/src/prisma/prisma.service.ts
@@ -1,11 +1,23 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '../../generated/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    const prismaExtended = new PrismaClient().$extends(withAccelerate());
+    super({
+      datasources: {
+        db: {
+          url: process.env.DATABASE_URL,
+        },
+      },
+    });
+    Object.assign(this, prismaExtended);
+  }
   async onModuleInit(): Promise<void> {
     const prisma: PrismaClient = this as PrismaClient;
     await prisma.$connect();
