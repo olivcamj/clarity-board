39667a2a5bf62ffea65d66c0256312dd0f73d086
Olivia Cameron <olivia.cameronj15@gmail.com>
2025-10-22 13:34:23 -0400

    fix (api): db migration out of sync reset

--- src/api/prisma/migrations/20250721005626_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Prompt" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Prompt_pkey" PRIMARY KEY ("id")
-);
