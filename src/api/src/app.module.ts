c43ac1f137e6b0688691aca16a0477b84e158884
Olivia Cameron <olivia.cameronj15@gmail.com>
2025-10-22 12:23:09 -0400

    feat (api): imports

--- src/api/src/app.module.ts
+++ src/api/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PromptsModule } from './prompts/prompts.module';
+import { UserModule } from './user/user.module';
+import { ClerkWebhookModule } from './webhooks/clerk-webhook.module';
 
 @Module({
-  imports: [PromptsModule],
+  imports: [PromptsModule, UserModule, ClerkWebhookModule],
   controllers: [AppController],
   providers: [AppService],
 })
