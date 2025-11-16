5d2dfcd153c2597ab8897dd717c540ec0a0da4a7
Olivia Cameron <olivia.cameronj15@gmail.com>
2025-11-16 11:47:36 -0500

    feat (api): list provider

--- src/api/src/user/user.module.ts
+++ src/api/src/user/user.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ClerkAuthGuard } from 'src/guards/clerk-auth.guard';
 
 @Module({
   imports: [PrismaModule],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, ClerkAuthGuard],
   exports: [UserService],
 })
 export class UserModule {}
