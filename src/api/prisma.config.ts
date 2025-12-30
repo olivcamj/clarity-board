fe605121c7bf557c82dae907babf91f86288040a
Olivia Cameron <olivia.cameronj15@gmail.com>
2025-12-30 09:24:50 -0500

    chore (api): update dep prisma

--- src/api/prisma.config.ts
+++ src/api/prisma.config.ts
@@ -1,7 +1,9 @@
-export default {
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
+import 'dotenv/config';
+import { defineConfig } from 'prisma/config';
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  datasource: {
+    url: process.env['DATABASE_URL'],
   },
-};
+});
