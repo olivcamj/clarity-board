e9fc870b19206541f8e9b691d709f2f140aef240
Olivia Cameron <olivia.cameronj15@gmail.com>
2025-12-30 09:23:49 -0500

    fix: update auth

--- src/api/src/guards/clerk-auth.guard.ts
+++ src/api/src/guards/clerk-auth.guard.ts
@@ -6,14 +6,12 @@ import {
 } from '@nestjs/common';
 import { verifyToken } from '@clerk/backend';
 import { Request as ExpressRequest } from 'express';
-import { PrismaService } from '../prisma/prisma.service';
 
 interface RequestWithAuth extends ExpressRequest {
   auth?: {
     userId: string;
     sessionId: string;
   };
-  user?: any;
   headers: {
     authorization?: string;
     [key: string]: any;
@@ -21,7 +19,7 @@ interface RequestWithAuth extends ExpressRequest {
 }
 @Injectable()
 export class ClerkAuthGuard implements CanActivate {
-  constructor(private prisma: PrismaService) {}
+  constructor() {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<RequestWithAuth>();
@@ -34,7 +32,7 @@ export class ClerkAuthGuard implements CanActivate {
     try {
       // Verify token with Clerk
       const payload = await verifyToken(token, {
-        secretKey: process.env.CLERK_SECRET_KEY,
+        secretKey: process.env.CLERK_SECRET_KEY!,
       });
       // Add user info to request object
       // This is where `req.auth` will be populated
@@ -42,24 +40,6 @@ export class ClerkAuthGuard implements CanActivate {
         userId: payload.sub, // The user ID is stored in the `sub` claim
         sessionId: payload.sid,
       };
-
-      const user = await this.prisma.user.findUnique({
-        where: { clerkId: payload.sub },
-        select: {
-          id: true,
-          clerkId: true,
-          name: true,
-          email: true,
-          role: true,
-        },
-      });
-
-      if (!user) {
-        throw new UnauthorizedException('User not found in database.');
-      }
-
-      request.user = user;
-
       return true;
     } catch (error) {
       console.error('Token verification failed:', error);
