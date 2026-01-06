0430732af9ec35d3cef9cf1385755ca2a5059fbf
Olivia Cameron <olivia.cameronj15@gmail.com>
2026-01-06 12:36:43 -0500

    fix (api): update args naming

--- src/api/src/user/user.controller.ts
+++ src/api/src/user/user.controller.ts
@@ -26,8 +26,7 @@ export class UserController {
    */
   @Get('me')
   async getCurrentUser(@CurrentUser() user: any) {
-    const clerkId = user.userId; // This is the Clerk user ID
-    return await this.userService.syncUserById(clerkId);
+    return await this.userService.syncUserById(user.clerkId);
   }
 
   /**
@@ -38,7 +37,7 @@ export class UserController {
    */
   @Get('me/full')
   async getCurrentUserWithRelations(@CurrentUser() user: any) {
-    return this.userService.getUserWithRelations(user.userId);
+    return this.userService.getUserWithRelations(user.clerkId);
   }
 
   /**
