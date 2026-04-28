49e5177c426091d547779d2cdf0209eb4e26b8bf
Olivia Cameron <olivia.cameronj15@gmail.com>
2026-04-28 13:07:37 -0400

    fix(api): add UserModule import to BoardModule for dependency injection

--- src/api/src/board/board.module.ts
+++ src/api/src/board/board.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BoardService } from './board.service';
 import { BoardController } from './board.controller';
+import { UserModule } from '../user/user.module';
 
 @Module({
+  imports: [UserModule],
   providers: [BoardService],
   controllers: [BoardController],
 })
