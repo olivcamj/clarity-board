ee45811634b21ccfdfb7c607f05a114a4ade5402
Olivia Cameron <olivia.cameronj15@gmail.com>
2026-07-02 12:22:23 -0400

    feat(app): replace AppSidebar with AppFrame in AppLayout component

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,17 +1,12 @@
 import { Suspense } from 'react';
-import { AppSidebar } from '../components/AppSidebar';
+import { AppFrame } from '../components/AppFrame';
 import { WorkspaceProvider } from '../lib/WorkspaceContext';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <Suspense>
       <WorkspaceProvider>
-        <div className="flex h-screen overflow-hidden">
-          <AppSidebar />
-          <main className="flex-1 overflow-y-auto">
-            {children}
-          </main>
-        </div>
+        <AppFrame>{children}</AppFrame>
       </WorkspaceProvider>
     </Suspense>
   );
